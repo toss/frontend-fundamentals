1726431813d9e0eabe5bdce0576388128e8814d1
yurim <yurim@toss.im>
2024-12-20 14:42:55 +0900

    Giscus 댓글 달기

---
+++ .vitepress/theme/components/comments.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import { useData } from 'vitepress'
+
+const { title } = useData()
+</script>
+
+<template>
+  <div :key="title" class="giscus">
+    <component
+      :is="'script'"
+      data-repo="toss/frontend-fundamentals"
+      data-repo-id="R_kgDONfHk5g"
+      data-category="Announcements"
+      data-category-id="DIC_kwDONfHk5s4ClUqT"
+      data-mapping="pathname"
+      data-strict="0"
+      data-reactions-enabled="1"
+      data-emit-metadata="0"
+      data-input-position="bottom"
+      data-theme="preferred_color_scheme"
+      data-lang="ko"
+      crossorigin="anonymous"
+      data-loading="lazy"
+      async
+    />
+  </div>
+</template>
