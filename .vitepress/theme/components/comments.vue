55539c46196be7c2e4a6ec41345b40bbf86aa89d
yurim <yurim@toss.im>
2025-01-07 17:42:56 +0900

    하하 제거

--- .vitepress/theme/components/comments.vue
+++ .vitepress/theme/components/comments.vue
@@ -6,7 +6,6 @@ const { frontmatter, title } = useData()
 
 <template>
   <div v-if="frontmatter.comments !== false" :key="title" class="giscus" style="margin-top: 24px;">
-    <div>하하</div>
     <component
       :is="'script'"
       data-repo="toss/frontend-fundamentals"
