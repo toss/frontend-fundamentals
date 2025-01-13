499691ec103ab00ff6e898af931f1b823c909a19
milooy <jayjinjay@gmail.com>
2025-01-13 16:44:03 +0900

    add amplitude

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -1,5 +1,11 @@
-import Layout from './Layout.vue'
+import Layout from "./Layout.vue";
+import * as amplitude from "@amplitude/analytics-browser";
 
 export default {
   Layout,
-}
+  async enhanceApp() {
+    const amplitudeApiKey = (import.meta as any).env.VITE_AMPLITUDE_API_KEY;
+
+    amplitude.init(amplitudeApiKey, { autocapture: true, logLevel: amplitude.Types.LogLevel.Debug });
+  }
+};
