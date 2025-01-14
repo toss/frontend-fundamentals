9f1577a3148dcef8f02c33501a4148b8834e7b2a
Yurim Jalynne Jin <jayjinjay@gmail.com>
2025-01-14 11:03:33 +0900

    docs: Initialize amplitude, update readme to English (#16)
    * fix: remove unnessesory license text
    
    * feat: update readme to en
    
    * feat: initialize amplitude

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -1,5 +1,12 @@
-import Layout from './Layout.vue'
+import Layout from "./Layout.vue";
+import * as amplitude from "@amplitude/analytics-browser";
 
 export default {
   Layout,
-}
+  async enhanceApp() {
+    if (typeof window !== 'undefined') {
+      const amplitudeApiKey = (import.meta as any).env.VITE_AMPLITUDE_API_KEY;
+      amplitude.init(amplitudeApiKey, { autocapture: true });
+    }
+  }
+};
