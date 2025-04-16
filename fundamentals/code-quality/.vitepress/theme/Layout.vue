0e61f92c11f54e2d03426a134a1209e0efb1fb19
Yurim Jalynne Jin <jayjinjay@gmail.com>
2025-04-16 12:01:22 +0900

    Add one navigation (#192)
    * feat: add one navigation
    
    * refactor: side-navbar -> one-navigation
    
    * change width
    
    * feat: navbar contents update
    
    * roll back styling
    
    * fea: final styling
    
    * feat: OneNavigation 컴포넌트 개선 및 아이템 구성 추가
    
    * feat: add coming soon
    
    * faet: update as adative color

--- fundamentals/code-quality/.vitepress/theme/Layout.vue
+++ fundamentals/code-quality/.vitepress/theme/Layout.vue
@@ -2,6 +2,7 @@
 import DefaultTheme from "vitepress/theme";
 import Comments from "./components/Comments.vue";
 import CustomBanner from "./components/CustomBanner.vue";
+import OneNavigation from "./components/OneNavigation.vue";
 import { useLocale } from "./composables";
 
 const { Layout } = DefaultTheme;
@@ -9,19 +10,37 @@ const { isKorean } = useLocale();
 </script>
 
 <template>
-  <Layout>
-    <template #doc-after>
-      <Comments />
-    </template>
-    <template #aside-bottom>
-      <CustomBanner v-if="isKorean" />
-    </template>
-  </Layout>
+  <OneNavigation />
+  <div class="layout-wrapper">
+    <Layout>
+      <template #doc-after>
+        <Comments />
+      </template>
+      <template #aside-bottom>
+        <CustomBanner v-if="isKorean" />
+      </template>
+    </Layout>
+  </div>
 </template>
 
 <style>
+:root {
+  --one-navi-width: 50px;
+}
 html {
   overflow-y: scroll;
   scrollbar-gutter: stable;
+}
+
+@media (min-width: 960px) {
+  .layout-wrapper {
+    padding-left: var(--one-navi-width);
+  }
+  .VPSidebar, .divider, .container > .title, .VPNavBar.has-sidebar .content {
+    margin-left: var(--one-navi-width);
+  }
+  .VPNavBar.has-sidebar > .title {
+    background: var(--vp-sidebar-bg-color) !important;
+  }
 }
 </style>
--- fundamentals/code-quality/.vitepress/theme/composables/index.ts
+++ fundamentals/code-quality/.vitepress/theme/composables/index.ts
@@ -1,2 +1,17 @@
-export * from "./useLocale";
+import { ref, computed } from "vue";
+import { useData } from "vitepress";
+
+export function useLocale() {
+  const { lang } = useData();
+
+  // 한국어 여부 확인
+  const isKorean = computed(() => {
+    return lang.value === "ko-KR" || lang.value === "ko";
+  });
+
+  return {
+    isKorean
+  };
+}
+
 export * from "./useBanner";
