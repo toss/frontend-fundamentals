475a6aecb3b1e4ffec26eba8516dd3ecbef9278c
Yurim Jalynne Jin <jayjinjay@gmail.com>
2025-05-16 10:27:24 +0900

    fix: Do absolute path in one navigation (#257)
    * fix:원네비에서 상대경로 아닌 절대경로로
    
    * fix: html 대신 js로 이동
    
    * feat: vercel.json 추가해서 404이슈 해결되길 기원하기
    
    * feat: 접근성 삭제
    
    * fix typo
    
    ---------
    
    Co-authored-by: donghyeon <donghyeon@toss.im>

--- fundamentals/shared/config/OneNavigationItems.ts
+++ fundamentals/shared/config/OneNavigationItems.ts
@@ -29,13 +29,13 @@ export const ONE_NAVIGATION_ITEMS: NavItem[] = [
       <polyline points="4,8 12,13 20,8" stroke="currentColor" stroke-width="2" fill="none"/>
       <rect x="11" y="3" width="2" height="10" fill="currentColor"/>`
   },
-  {
-    path: "/accessibility",
-    href: "/code-quality/code/coming-soon",
-    tooltip: {
-      ko: "접근성",
-      en: "Accessibility"
-    },
-    icon: `<path fill-rule="evenodd" d="M14.447 3.027a.75.75 0 01.527.92l-4.5 16.5a.75.75 0 01-1.448-.394l4.5-16.5a.75.75 0 01.921-.526zM16.72 6.22a.75.75 0 011.06 0l5.25 5.25a.75.75 0 010 1.06l-5.25 5.25a.75.75 0 11-1.06-1.06L21.44 12l-4.72-4.72a.75.75 0 010-1.06zm-9.44 0a.75.75 0 010 1.06L2.56 12l4.72 4.72a.75.75 0 11-1.06 1.06L.97 12.53a.75.75 0 010-1.06l5.25-5.25a.75.75 0 011.06 0z" clip-rule="evenodd"/>`
-  }
+  // {
+  //   path: "/accessibility",
+  //   href: "/code-quality/code/coming-soon",
+  //   tooltip: {
+  //     ko: "접근성",
+  //     en: "Accessibility"
+  //   },
+  //   icon: `<path fill-rule="evenodd" d="M14.447 3.027a.75.75 0 01.527.92l-4.5 16.5a.75.75 0 01-1.448-.394l4.5-16.5a.75.75 0 01.921-.526zM16.72 6.22a.75.75 0 011.06 0l5.25 5.25a.75.75 0 010 1.06l-5.25 5.25a.75.75 0 11-1.06-1.06L21.44 12l-4.72-4.72a.75.75 0 010-1.06zm-9.44 0a.75.75 0 010 1.06L2.56 12l4.72 4.72a.75.75 0 11-1.06 1.06L.97 12.53a.75.75 0 010-1.06l5.25-5.25a.75.75 0 011.06 0z" clip-rule="evenodd"/>`
+  // }
 ]; 
