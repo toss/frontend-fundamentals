f7f89ccd28a3a0d4e718d24c2f15fcf320ae1b6c
yurimjin <jayjinjay@gmail.com>
2025-09-21 15:46:50 +0900

    feat: legacy 삭제

--- fundamentals/today-i-learned/src/pages/profile/components/ProfileHeader.tsx
+++ fundamentals/today-i-learned/src/pages/profile/components/ProfileHeader.tsx
@@ -1,18 +1,23 @@
 import { ProfileInfo } from "@/components/features/profile/ProfileInfo";
-import { cn } from "@/libs/utils";
+import { css, cx } from "@styled-system/css";
 import type { BaseComponentProps } from "@/types";
 
 interface ProfileHeaderProps extends BaseComponentProps {}
 
 export function ProfileHeader({ className }: ProfileHeaderProps) {
   return (
-    <div
-      className={cn(
-        "flex flex-col items-center py-4 px-4 gap-6 relative",
-        className
-      )}
-    >
+    <div className={cx(profileHeaderContainer, className)}>
       <ProfileInfo />
     </div>
   );
 }
+
+const profileHeaderContainer = css({
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  paddingY: '1rem',
+  paddingX: '1rem',
+  gap: '1.5rem',
+  position: 'relative'
+});
