381644ac715dc25b31928ce253e8a2e5b8fffad6
sungun <gorjs3540@gmail.com>
2025-09-29 01:08:34 +0900

    refactor: 비로그인 상태 시 content를 figma와 동일하게 수정

--- fundamentals/today-i-learned/src/components/features/auth/UnauthenticatedState.tsx
+++ fundamentals/today-i-learned/src/components/features/auth/UnauthenticatedState.tsx
@@ -1,25 +1,48 @@
 import { useAuth } from "@/contexts/AuthContext";
 import { Button } from "@/components/shared/ui/Button";
+import { css } from "@styled-system/css";
 
 export function UnauthenticatedState() {
   const { login } = useAuth();
 
   return (
-    <div className="flex flex-col items-center justify-center px-6 py-8 gap-4 w-full rounded-2xl bg-white">
-      {/* 메인 텍스트 */}
-      <h1 className="text-xl font-bold text-center text-black/80 tracking-tight leading-[130%]">
-        매일 한 줄, 오늘 배운 내용을 기록해봐요!
-      </h1>
+    <div className={container}>
+      <span className={title}>오늘 배운 내용을 기록하려면 로그인해 주세요</span>
 
-      {/* 버튼 */}
-      <Button
-        onClick={login}
-        variant="primary"
-        size="lg"
-        className="px-8 py-3 text-base font-bold"
-      >
+      <Button onClick={login} variant="primary" size="lg" className={button}>
         로그인하기
       </Button>
     </div>
   );
 }
+
+const container = css({
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  justifyContent: "center",
+  paddingX: "2rem",
+  paddingY: "3rem",
+  gap: "1.5rem"
+});
+
+const title = css({
+  fontFamily: "Toss Product Sans OTF",
+  fontSize: "1.125rem",
+  fontWeight: "700",
+  fontStyle: "bold",
+  lineHeight: "130%",
+  letterSpacing: "-0.025em"
+});
+
+const button = css({
+  maxHeight: "2.5rem",
+  paddingX: "1rem",
+
+  fontFamily: "Toss Product Sans OTF",
+  fontSize: "1rem",
+  fontWeight: "700",
+  fontStyle: "bold",
+  lineHeight: "130%",
+  letterSpacing: "-0.025em"
+});
